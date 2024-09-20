3656e40379f35d69ae32c5840ce0a45b8319015c
Axis <erlia@naver.com>
2024-09-21 05:11:23 +0900

    1

--- src/main/java/ServerBox/AxisServerBox/controller/TestController.java
+++ src/main/java/ServerBox/AxisServerBox/controller/TestController.java
@@ -1,4 +1,22 @@
 package ServerBox.AxisServerBox.controller;
 
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping(value = "/api/server")
 public class TestController {
+   // private final Logger LOGGER= LoggerFactory.getLogger(TestController.class);
+
+    @GetMapping(value = "axis")
+    public String getTest(){
+        return "Hello Axis from AxisServerBox";
+
+    }
+
+
 }

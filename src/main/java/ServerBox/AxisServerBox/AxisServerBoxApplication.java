a439bdd98ccd991c1b99292e7acbd395f86ed9fa
Axis <erlia@naver.com>
2024-09-21 04:53:35 +0900

    1

---
+++ src/main/java/ServerBox/AxisServerBox/AxisServerBoxApplication.java
@@ -0,0 +1,13 @@
+package ServerBox.AxisServerBox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxisServerBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxisServerBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ServerBox/AxisServerBox/AxisServerBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package ServerBox.AxisServerBox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxisServerBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
